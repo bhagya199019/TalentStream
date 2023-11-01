66f85940c79cd3af26d158db3623b34db4190cef
bhagya199019 <bhagya092020@gmail.com>
2023-11-01 12:05:35 +0530

    commit

--- MyTalentTracker1/src/main/java/com/bitlabs/App/Controller/ApplicantProfileController.java
+++ MyTalentTracker1/src/main/java/com/bitlabs/App/Controller/ApplicantProfileController.java
@@ -1,5 +1,42 @@
 package com.bitlabs.App.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.bitlabs.App.Entity.ApplicantProfile;
+import com.bitlabs.App.Entity.JobApplicant;
+import com.bitlabs.App.Repository.JobApplicantRepository;
+import com.bitlabs.App.Service.ApplicantProfileService;
+
 public class ApplicantProfileController {
 
+	@Autowired
+ private JobApplicantRepository jobApplicantRepository;
+	
+	@Autowired
+ private ApplicantProfileService applicantProfileService;
+	
+@PostMapping("/applicant/createProfile/{applicantId}")
+public ResponseEntity<ApplicantProfile>createOrUpdateProfile(@RequestBody ApplicantProfile applicantProfile,@PathVariable long applicantId){
+	
+	JobApplicant jobApplicant=jobApplicantRepository.getJobApplicantById(applicantId);
+	
+	if(jobApplicant==null) {
+		return ResponseEntity.notFound().build();
+	}
+	else {
+		applicantProfile.setApplicant(jobApplicant);
+		ApplicantProfile createdProfile=applicantProfileService.createOrUpdateProfile(applicantProfile);
+		return ResponseEntity.ok(createdProfile);
+		
+	}
+}
+	
+	
+	
+	
+	
 }
