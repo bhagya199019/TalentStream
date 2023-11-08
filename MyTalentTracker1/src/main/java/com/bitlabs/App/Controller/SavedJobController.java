fa64b5aaba346c6977568c7154a8996a74e3e53b
bhagya199019 <bhagya092020@gmail.com>
2023-11-08 12:58:34 +0530

    commited

---
+++ MyTalentTracker1/src/main/java/com/bitlabs/App/Controller/SavedJobController.java
@@ -0,0 +1,30 @@
+package com.bitlabs.App.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bitlabs.App.Service.SavedJobService;
+
+@RestController
+public class SavedJobController {
+
+  @Autowired
+ private SavedJobService savedJobService; 
+
+	 @PostMapping("/applicant/saveJob/{applicantId}/{jobId}")
+  public ResponseEntity<String>saveJobByApplicant(@PathVariable long applicantId,@PathVariable  long jobId){
+		 
+		 try {
+			 	 savedJobService.saveJobForApplicant(applicantId,jobId);
+			 	 return ResponseEntity.ok("job saved successfully");
+	 }
+		 catch(Exception e) {
+			 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+			 
+		 }
+}
+}
