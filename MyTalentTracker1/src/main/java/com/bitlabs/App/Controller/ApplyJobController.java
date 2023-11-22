f016fa47a1689930cb8edaec75e59bc16cc17c3d
bhagya199019 <bhagya092020@gmail.com>
2023-11-22 10:55:25 +0530

    commit

--- MyTalentTracker1/src/main/java/com/bitlabs/App/Controller/ApplyJobController.java
+++ MyTalentTracker1/src/main/java/com/bitlabs/App/Controller/ApplyJobController.java
@@ -3,8 +3,11 @@ package com.bitlabs.App.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.bitlabs.App.Service.ApplyJobservice;
 
+@RestController
 public class ApplyJobController {
 
 	@Autowired
